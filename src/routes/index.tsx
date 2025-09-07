c092fcf354afb2c0e962b17429022d03f2746956
jvcbyte <jvc8463@gmail.com>
2025-09-07 20:10:07 +0100

    add shadcn btn n input

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -7,6 +9,30 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="text-center">
+      <h1>ToDo With Viem</h1>
+
+      <div className="flex justify-center flex-wrap gap-6">
+
+        <div className="flex flex-col w-1/3 border border-gray-200 p-2 rounded-md">
+          <label htmlFor="" className="text-left">Add a todo</label>
+          <div className="flex flex-col gap-4">
+            <Input type='text' placeholder='Add a todo' />
+            <Button>Submit</Button>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-1/3 border border-gray-200 p-2 rounded-md">
+          <Input type='text' placeholder='Add a todo' />
+        </div>
+
+        <div className="flex flex-col w-1/3 border border-gray-200 p-2 rounded-md">
+          <Input type='text' placeholder='Add a todo' />
+        </div>
+
+        <div className="flex flex-col w-1/3 border border-gray-200 p-2 rounded-md">
+          <Input type='text' placeholder='Add a todo' />
+        </div>
+      </div>
      
     </div>
   )
